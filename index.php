48582dd66ba8135a1765a0e951df72f514cf930c
ruchithasuvarna <70750565+ruchithasuvarna@users.noreply.github.com>
2021-08-18 11:07:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+include_once 'connect_db.php';
+if(isset($_POST['submit'])){
+$username=$_POST['username'];
+$password=$_POST['password'];
+$position=$_POST['position'];
+switch($position){
+case 'Admin':
+$result=mysql_query("SELECT admin_id, username FROM admin WHERE username='$username' AND password='$password'");
+$row=mysql_fetch_array($result);
+if($row>0){
+session_start();
+$_SESSION['admin_id']=$row[0];
+$_SESSION['username']=$row[1];
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/admin.php");
+}else{
+$message="<font color=red>Invalid login</font>";
+}
+break;
+case 'Pharmacist':
+$result=mysql_query("SELECT pharmacist_id, first_name,last_name,staff_id,username FROM pharmacist WHERE username='$username' AND password='$password'");
+$row=mysql_fetch_array($result);
+if($row>0){
+session_start();
+$_SESSION['pharmacist_id']=$row[0];
+$_SESSION['first_name']=$row[1];
+$_SESSION['last_name']=$row[2];
+$_SESSION['staff_id']=$row[3];
+$_SESSION['username']=$row[4];
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/pharmacist.php");
+}else{
+$message="<font color=red>Invalid login</font>";
+}
+break;
+case 'Cashier':
+$result=mysql_query("SELECT cashier_id, first_name,last_name,staff_id,username FROM cashier WHERE username='$username' AND password='$password'");
+$row=mysql_fetch_array($result);
+if($row>0){
+session_start();
+$_SESSION['cashier_id']=$row[0];
+$_SESSION['first_name']=$row[1];
+$_SESSION['last_name']=$row[2];
+$_SESSION['staff_id']=$row[3];
+$_SESSION['username']=$row[4];
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/cashier.php");
+}else{
+$message="<font color=red>Invalid login</font>";
+}
+break;
+case 'Manager':
+$result=mysql_query("SELECT manager_id, first_name,last_name,staff_id,username FROM manager WHERE username='$username' AND password='$password'");
+$row=mysql_fetch_array($result);
+if($row>0){
+session_start();
+$_SESSION['manager_id']=$row[0];
+$_SESSION['first_name']=$row[1];
+$_SESSION['last_name']=$row[2];
+$_SESSION['staff_id']=$row[3];
+$_SESSION['username']=$row[4];
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/manager.php");
+}else{
+$message="<font color=red>Invalid login</font>";
+}
+break;
+}}
+echo <<<LOGIN
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#content {
+text-align: center;
+height: auto;
+}
+#main{
+height: auto;
+}
+</style>
+</head>
+<link rel="stylesheet" type="text/css" href="style/pmsc.css">
+<body>
+<div id="content">
+<div id="header">
+ <img src="images/hd_logo.jpg" height="150" width="200">
+</div>
+<div id="main">
+  <section class="container">
+     <div class="login">
+      <h1><emp><b>Pharmacy Management System</emp></b></h1>
+	  $message
+      <form method="post" action="index.php">
+		 <p><input type="text" name="username" value="" placeholder="Username"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+		<p><select name="position">
+		<option>--Select position--</option>
+			<option>Admin</option>
+			<option>Pharmacist</option>
+			<option>Cashier</option>
+			<option>Manager</option>
+			</select></p>
+        <p class="submit"><input type="submit" name="submit" value="Login"></p>
+      </form>
+    </div>
+    </section>
+</div>
+<div id="footer" align="Center"></div>
+</div>
+</body>
+</html>
+LOGIN;
+?>
